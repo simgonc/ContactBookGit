d685eeb966adc1e512bf53b9098a003d8a52d334
Simao <sl.goncalves@campus.fct.unl.pt>
2026-09-18 20:36:24 +0100

    Added the commands in main

--- src/Main.java
+++ src/Main.java
@@ -25,8 +25,8 @@ public class Main {
     public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
     public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
-    public static final String BOOK_NO_DUPS = "There are contacts that share phone numbers.";
-    public static final String BOOK_HAS_DUPS = "All contacts have different phone numbers.";
+    public static final String BOOK_HAS_DUPS = "There are contacts that share phone numbers.";
+    public static final String BOOK_NO_DUPS = "All contacts have different phone numbers.";
     public static final String NO_CONTACT = "Phone number does not exist.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
